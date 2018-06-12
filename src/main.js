039ecc290582910ea09f1e725311de28ba582d8d
Maria <mariaanguevara@gmail.com>
2018-06-12 18:17:26 -0500

    subiendo avance para gestionar

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+ let one="";
 fetch('http://127.0.0.1:5500/data/cohorts.json')
   .then(function(response) {
     return response.json();
   })
+  
   .then(function(data) {
-    console.log(data);
-  });
+    console.log(data[0].id);
+    console.log(data[1].id)
+    let one =(data[0].id);
+    document.getElementById("test").innerHTML=one;
+  })    
+  
+  
+  
+  document.getElementById("1").addEventListener("click",()=>
+  {
+    alert("diste click en lima");
+  })
+  
