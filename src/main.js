cee232d125fc4f7478c1979b259159d5425e0a0c
Maria <mariaanguevara@gmail.com>
2018-06-13 17:34:52 -0500

    combio a enlace

--- src/main.js
+++ src/main.js
@@ -1,23 +1,19 @@
- let one="";
+ let list="";
 fetch('http://127.0.0.1:5500/data/cohorts.json')
   .then(function(response) {
     return response.json();
   })
-  
   .then(function(data) {
-    console.log(data[0].id);
-    console.log(data[1].id);
+    //console.log(data[0].id);
+    //console.log(data[1].id);
     
-    document.getElementById("items").innerHTML+="<form>";
+    document.getElementById("items").innerHTML+="<select>";
     for(let i=0; i<data.length; i++){
       console.log(data[i].id)
-      let one =(data[i].id);
+      let list =(data[i].id);
       document.getElementById("items").innerHTML+="<option >"+one+"</option>";
     }
-    document.getElementById("items").innerHTML+="</form>";
-
-
-    
+    document.getElementById("items").innerHTML+="</select>";  
   })    
   
   
