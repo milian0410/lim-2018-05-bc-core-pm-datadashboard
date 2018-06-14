e97b8834ee1079cdfdfbf87b28b6cc3d5015e07d
milian0410 <milian0410@hotmail.com>
2018-06-14 18:38:03 -0500

    insertando data

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
- let one="";
+let one="";
+let selectionOption = document.getElementById("listcohorts");
+
+
 fetch('http://127.0.0.1:5500/data/cohorts.json')
   .then(function(response) {
     return response.json();
@@ -11,14 +14,23 @@ fetch('http://127.0.0.1:5500/data/cohorts.json')
     for(let i=0; i<data.length; i++){
       console.log(data[i].id)
       let one =(data[i].id);
-      document.getElementById("listcohorts").innerHTML+="<option >"+one+"</option>";
+      document.getElementById("listcohorts").innerHTML+="<option>"+one+"</option>";
 
+      
     }     
   })    
-
-
   
-  fetch('http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/users.json')
+  selectionOption.setAttribute("value","listcohorts[i]");
+ // console.log(listcohorts.value);
+  selectionOption.addEventListener("change", (event)=>{
+  //  console.log(event.target.value);
+   })
+   let listLima = document.getElementById("list");
+   if(e.target.value === "lim-2018-03-pre-core-pw"){
+   listLima.innerHTML+= 'http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/users.json'
+
+
+/*  fetch('http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/users.json')
   .then(function(response) {
     return response.json();
   })
@@ -33,11 +45,7 @@ fetch('http://127.0.0.1:5500/data/cohorts.json')
       let one =(data[i].name);
       document.getElementById("listusers").innerHTML+="<option >"+one+"</option>";
     }
-    
-
-
-    
-  })    
+  })   */ 
   
   
   
