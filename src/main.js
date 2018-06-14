b884d8e262953659a31febe84e3ce53b6ed2b7b9
Maria <mariaanguevara@gmail.com>
2018-06-14 18:34:36 -0500

    agregarle condicional al envento cohort

--- src/main.js
+++ src/main.js
@@ -1,23 +1,56 @@
  let one="";
+ let selection=document.getElementById("listcohorts");
+ 
 fetch('http://127.0.0.1:5500/data/cohorts.json')
-  .then(function(response) {
+  .then(function(response)
+   {
     return response.json();
-  })
+   })
   
-  .then(function(data) {
-    console.log(data[0].id);
-    console.log(data[1].id);
-    
-    for(let i=0; i<data.length; i++){
-      console.log(data[i].id)
+  .then(function(data) 
+   {
+    for(let i=0; i<data.length; i++)
+    {
       let one =(data[i].id);
-      document.getElementById("listcohorts").innerHTML+="<option >"+one+"</option>";
-
+      document.getElementById("listcohorts").innerHTML+="<option value="+i+" >"+one+"</option>";
+      document.getElementsByTagName("option");
     }     
-  })    
-
-
+   })  
   
+  selection.addEventListener("change", (event)=>
+  {
+     
+    document.getElementById("prueba").innerHTML+=
+        fetch('http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/users.json')
+        .then(function(response)
+         {
+          return response.json();
+         })        
+        .then(function(data)
+         { 
+            for(let i=0; i<data.length; i++)
+            {
+              let one =(data[i].name);
+              document.getElementById("listusers").innerHTML+="<option value="+i+">"+one+"</option>";
+            }  
+          })    
+  }) 
+  
+  
+  
+   // console.log(data[i].name)
+  // console.log(data[0].id);
+    //console.log(data[1].id);
+    
+ // console.log(data[i].id)
+//console.log(document.getElementsByTagName("option"));
+  // console.log(data[0].name);
+          //console.log(data[1].name);
+   // console.log("me diste click somos value 31")
+
+/*
+  selectionOption.setAttrivute("value","listcohorts[i]");
+    
   fetch('http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/users.json')
   .then(function(response) {
     return response.json();
@@ -31,7 +64,7 @@ fetch('http://127.0.0.1:5500/data/cohorts.json')
     for(let i=0; i<data.length; i++){
       console.log(data[i].name)
       let one =(data[i].name);
-      document.getElementById("listusers").innerHTML+="<option >"+one+"</option>";
+      document.getElementById("listusers").innerHTML+="<option value="+i+">"+one+"</option>";
     }
     
 
@@ -39,7 +72,6 @@ fetch('http://127.0.0.1:5500/data/cohorts.json')
     
   })    
   
-  
-  
-  
-  
+  */
+
+ 
