d5ed41aee2fc62d710d051b1fee257f053342906
Maria <mariaanguevara@gmail.com>
2018-06-15 14:02:27 -0500

    solo con el cohort lim2018pw sale la lista de user

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
  let one="";
- let selection=document.getElementById("listcohorts");
+ const selection=document.getElementById("listcohorts");
  
 fetch('http://127.0.0.1:5500/data/cohorts.json')
   .then(function(response)
@@ -12,16 +12,20 @@ fetch('http://127.0.0.1:5500/data/cohorts.json')
     for(let i=0; i<data.length; i++)
     {
       let one =(data[i].id);
-      document.getElementById("listcohorts").innerHTML+="<option value="+i+" >"+one+"</option>";
+      selection.innerHTML+="<option value="+data[i].id+" >"+one+"</option>";
       document.getElementsByTagName("option");
     }     
    })  
   
   selection.addEventListener("change", (event)=>
-  {
+
+  {console.log(selection.value);
+    if(selection.value==="lim-2018-03-pre-core-pw"){
      
-    document.getElementById("prueba").innerHTML+=
+    
+
         fetch('http://127.0.0.1:5500/data/cohorts/lim-2018-03-pre-core-pw/users.json')
+
         .then(function(response)
          {
           return response.json();
@@ -32,8 +36,20 @@ fetch('http://127.0.0.1:5500/data/cohorts.json')
             {
               let one =(data[i].name);
               document.getElementById("listusers").innerHTML+="<option value="+i+">"+one+"</option>";
+              document.getElementById("prueba").innerHTML="Aquí van porcentajes";
             }  
-          })    
+         })
+        } else if(selection.value!=="lim-2018-03-pre-core-pw")
+        {
+        
+        document.getElementById("prueba").innerHTML="Aún no hay datos";
+        document.getElementById("listusers").innerHTML+="<option value="+i+">"+"aun no hay datos"+"</option>";
+        } 
+      else{
+        
+      } 
+    
+       
   }) 
   
   
